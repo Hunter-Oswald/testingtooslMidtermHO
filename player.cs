810065fe0a87137636f7fd4c9ccd317b61ebed93
Hunter-Oswald <hunter-o@outlook.com>
2022-10-19 15:47:23 -0400

    Add project files.

---
+++ player.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace testingtooslMidtermHO
+{
+    internal class player
+    {
+        String firstName;
+        String lastName;
+        String playerID;
+        int score;
+    }
+}
